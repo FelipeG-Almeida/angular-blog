bfcb791565a9f99c6fdb26a172dc511fac7daa2d
Felipe <felipegustavotheboss@gmail.com>
2023-10-13 07:27:03 -0300

    Added dinamicaly input inside components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent {
-
+  @Input()
+  photoCover: String = '';
+  @Input()
+  cardTitle: String = '';
+  @Input()
+  cardDescription: String = '';
 }
